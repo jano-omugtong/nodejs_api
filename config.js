42c2a3d4f684a7309a7ee796905871b132ca5f7f
jano.omugtong <Jano.Omugtong@ibm.com>
2020-02-05 14:14:12 +0800

    env minor changes

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ let config = {};
 
 config.defaultPass = 'default';
 config.activateUrl = 'http://localhost:3000/auth/signup/activate/';
-config.gmailUsername = 'saasteamaws'; // place gmail account username here
-config.gmailPassword = '12angDum^^y'; // place gmail account password here
+config.gmailUsername = ''; // place gmail account username here
+config.gmailPassword = ''; // place gmail account password here
 
 module.exports = config;
